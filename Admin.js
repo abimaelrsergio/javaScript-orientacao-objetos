0bd39cba4d48e9721c3526c60fe4e5a1a8b998cd
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-24 17:18:25 -0300

    Classes e herança: Adicionando métodos

--- Admin.js
+++ Admin.js
@@ -4,8 +4,13 @@ class Admin extends User {
   constructor(nome, email, nascimento, role = 'admin', ativo = true) {
     super(nome, email, nascimento, role, ativo)
   }
+
+  criarCurso(nomeCurso, vagas){
+    return `Curso de ${nomeCurso} criado com ${vagas} vagas.`
+  }
 }
 
 const novoAdmin = new Admin('Sergio', 'r@r.com', '2021-01-01');
+console.log(novoAdmin.criarCurso('JS', 20))
 console.log(novoAdmin)
 console.log(novoAdmin.exibirInfos())
