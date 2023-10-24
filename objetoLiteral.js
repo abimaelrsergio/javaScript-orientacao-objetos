ebc5cb5e0a80397e6064671bc359d28855c4ee0f
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-24 15:19:08 -0300

    Cadeia de protótipo

--- objetoLiteral.js
+++ objetoLiteral.js
@@ -2,35 +2,25 @@ const user = {
     nome: 'Abimael',
     email: 'abimael@gmail.com',
     nascimento: '2021/01/01',
-    role: 'admin',
+    role: 'estudante',
     ativo: true,
     exibirInfos: function() {
         return console.log(this.nome, this.email)
     }
 }
 
-//user.exibirInfos();
-// a função perdeu o contexto, perdeu a conexão om o objeto user.
-// O this não funciona na variável exibir
-//const exibir = user.exibirInfos;
-//exibir();
-
-// é o mesmo que:
-//const exibir = function() {
-    //return console.log(this.nome, this.email)
-//}
-//exibir();
-
-// outro exemplo que traz o objeto global, contexto global
-//const exibir = function() {
-//    return console.log(this)
-//}
-//exibir();
-
-// Prender a função em um contexto usando bind()
-const exibir = function() {
-    return console.log(this.nome)
+const admin = {
+    nome: 'Mariana',
+    email: 'm@m.com',
+    role: 'admin',
+    criarCurso() {
+        console.log('curso criado')
+    }
 }
-const exibirNome = exibir.bind(user);
-exibirNome();
-exibir();
+
+// Parametro 1: Objeto que vai herdar propriedades (admin)
+// Parametro 2: Objeto que vai ceder as propriedades (user)
+Object.setPrototypeOf(admin, user);
+
+admin.criarCurso();
+admin.exibirInfos();
