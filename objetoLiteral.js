2847468823472261f2446e375b100f6c09d69c9c
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-24 13:50:33 -0300

    Entendendo o this

--- objetoLiteral.js
+++ objetoLiteral.js
@@ -4,9 +4,33 @@ const user = {
     nascimento: '2021/01/01',
     role: 'admin',
     ativo: true,
-    exibirInfor: function() {
+    exibirInfos: function() {
         return console.log(this.nome, this.email)
     }
 }
 
-user.exibirInfor();
+//user.exibirInfos();
+// a função perdeu o contexto, perdeu a conexão om o objeto user.
+// O this não funciona na variável exibir
+//const exibir = user.exibirInfos;
+//exibir();
+
+// é o mesmo que:
+//const exibir = function() {
+    //return console.log(this.nome, this.email)
+//}
+//exibir();
+
+// outro exemplo que traz o objeto global, contexto global
+//const exibir = function() {
+//    return console.log(this)
+//}
+//exibir();
+
+// Prender a função em um contexto usando bind()
+const exibir = function() {
+    return console.log(this.nome)
+}
+const exibirNome = exibir.bind(user);
+exibirNome();
+exibir();
