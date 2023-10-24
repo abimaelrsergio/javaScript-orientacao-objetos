7419a36785a6ca77d8e0018b97a1c5834d67ed0b
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-24 17:25:07 -0300

    Classes e herança: Métodos estáticos

--- Docente.js
+++ Docente.js
@@ -8,9 +8,15 @@ class Docente extends User {
   aprovaEstudante(estudante, curso) {
     return `Estudante ${estudante} aprovado no cruso ${curso}`
   }
+
+  // o método é static e é executado a partir da própria classe, e não de uma instância dela
+  static exibirRole() {
+    return `A role do docente é: ${this.role}`
+  }
 }
 
 const novoDocente = new Docente('Mariana', 'm@m.com', '2021-01-01');
 console.log(novoDocente)
 console.log(novoDocente.exibirInfos())
 console.log(novoDocente.aprovaEstudante('Juliana', 'JS'))
+console.log(Docente.exibirRole())
