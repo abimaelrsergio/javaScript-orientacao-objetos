5216a6fd1b8c7b1ce8e91658c0139e98abfaf665
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-25 15:07:27 -0300

    Encapsulamento: Métodos privados

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import User from './User.js';
 import Docente from './Docente.js';
-import Admim from './Admin.js';
+import Admin from './Admin.js';
 
 const novoUser = new User('Mariana', 'm@m.com', '2021-01-02');
 console.log(novoUser.exibirInfos());
+
+const novoAdmin = new Admin('Sergio', 'a@b.com', '2021-02-04')
+console.log(novoAdmin.exibirInfos());
